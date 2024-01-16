274530d1092c53fb24547b438459f03ca1b5521d
Stephanie-Y-Lopez <145513219+Stephanie-Y-Lopez@users.noreply.github.com>
2024-01-16 12:33:13 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GA_MethodOverloading_StephanieLopez
+{
+    internal class Program
+    {
+        //Stephanie Lopez
+        static void Main(string[] args)
+        {
+            //Method overloading is a feature that allows classes to define multiple methods with the same name but different parameters. 
+            //Method overloading works by two or more methods in the same class sharing the same name 
+        }
+    }
+}
